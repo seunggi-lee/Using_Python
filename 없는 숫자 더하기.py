32a14ec2e47b64cd9988565c2e9a49f36e8cd7ce
이승기 <won9704@naver.com>
2022-02-27 20:46:19 +0900

    프로그래머스 Lv1) 없는 숫자 더하기

---
+++ 없는 숫자 더하기.py
@@ -0,0 +1,11 @@
+def solution(numbers):
+    answer = 0
+
+    for num in range(10):
+        if num not in numbers:
+            answer += num
+        else:
+            pass
+
+    return answer
+
