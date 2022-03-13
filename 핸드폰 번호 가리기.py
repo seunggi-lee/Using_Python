60ea34840170bf05e31d8ac17bef972a162b0727
이승기 <won9704@naver.com>
2022-03-13 14:37:33 +0900

    프로그래머스 Lv1) 핸드폰 번호 가리기

--- 핸드폰 번호 가리기.py
+++ 핸드폰 번호 가리기.py
@@ -1,13 +1,13 @@
-# def solution(phone_number):
-#     answer = ''
-#     length = len(phone_number) - 5
-#     for i in range(len(phone_number)):
-#         if i <= length:
-#             answer = answer + '*'
-#         else:
-#             answer += phone_number[i]
-#
-#     return answer
-
 def solution(phone_number):
-    return '*' * (len(phone_number) - 4) + phone_number[-4 : ]
+    answer = ''
+    length = len(phone_number) - 5
+    for i in range(len(phone_number)):
+        if i <= length:
+            answer = answer + '*'
+        else:
+            answer += phone_number[i]
+
+    return answer
+
+# def solution(phone_number):
+#     return '*' * (len(phone_number) - 4) + phone_number[-4 : ]
