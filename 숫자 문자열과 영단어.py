62459aa53a66eef284fc2a6f8b2367c3bd9df78f
이승기 <won9704@naver.com>
2022-03-25 22:10:51 +0900

    프로그래머스 Lv1) 숫자 문자열과 영단어 - 보완

--- 숫자 문자열과 영단어.py
+++ 숫자 문자열과 영단어.py
@@ -1,10 +1,5 @@
 def solution(s):
-    answer = s
     num_list = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     for i in range(len(num_list)):
-        if num_list[i] in s:
-            answer.replace(num_list[i], str(i))
-            print(answer)
-    return answer
-
-solution("one4seveneight")
+        s = s.replace(num_list[i], str(i))
+    return int(s)
