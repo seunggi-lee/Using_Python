2b55be26f20e31f9a0d3d2b41fcba1e2ca6d86d5
이승기 <won9704@naver.com>
2022-04-08 21:11:06 +0900

    프로그래머스 Lv2) 구명보트 - 수정해야함

---
+++ 구명보트.py
@@ -0,0 +1,35 @@
+def solution(people, limit):
+    answer = 0
+    temp = 0
+    min_weight = 40
+    for idx, val in enumerate(people):
+        temp_weight = 0
+        if idx != len(people) - 1:
+            if val + min_weight >= limit:
+                answer += 1
+                people[idx] = limit + 1
+                continue
+
+            elif people[idx] > limit:
+                continue
+
+            else:
+                temp_weight = val
+                for j in range(idx + 1, len(people)):
+                    if temp_weight + people[j] <= limit:
+                        temp_weight += people[j]
+                        people[j] = limit + 1
+                        if temp_weight + min_weight >= limit:
+                            answer += 1
+                            break
+                        else:
+                            continue
+                    else:
+                        continue
+        else:
+            if val > limit:
+                break
+            else:
+                answer += 1
+    print(people)
+    return answer
