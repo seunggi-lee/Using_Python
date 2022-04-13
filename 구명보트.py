2cc913874ac7ecababd8ca8977ef211e13813f89
이승기 <won9704@naver.com>
2022-04-13 21:24:35 +0900

    프로그래머스 Lv2) 구명보트 - 수정완료

--- 구명보트.py
+++ 구명보트.py
@@ -1,32 +1,50 @@
+# 답은 맞으나 효율성 1~3 만족 못함
+# def solution(people, limit):
+#     answer = 0
+#     people.sort(reverse=True)
+#     temp = 0
+#     for i, val in enumerate(people):
+#         if val + people[-1] > limit:
+#             answer += 1
+#             people[i] = limit + 1
+#         else:
+#             temp = i
+#             break
+#     for j in range(temp, len(people)):
+#         if people[j] <= limit:
+#             temp_weight = people[j]
+#             people[j] = limit + 1
+#             count = j + 1
+#             answer += 1
+#             while count < len(people):
+#                 if temp_weight + people[count] > limit:
+#                     count += 1
+#                     continue
+#                 else:
+#                     people[count] = limit + 1
+#                     break
+#
+#     return answer
+
 def solution(people, limit):
     answer = 0
-    min_weight = 40
-    for idx, val in enumerate(people):
-        if idx != len(people) - 1:
-            if val + min_weight >= limit:
-                answer += 1
-                people[idx] = limit + 1
-                continue
-
-            elif people[idx] > limit:
-                continue
-
-            else:
-                temp_weight = val
-                for j in range(idx + 1, len(people)):
-                    if temp_weight + people[j] <= limit:
-                        temp_weight += people[j]
-                        people[j] = limit + 1
-                        if temp_weight + min_weight >= limit:
-                            answer += 1
-                            break
-                        else:
-                            continue
-                    else:
-                        continue
+    people.sort(reverse=True)
+    temp = 0
+    for i, val in enumerate(people):
+        if val + people[-1] > limit:
+            answer += 1
+            people[i] = limit + 1
         else:
-            if val > limit:
-                break
+            temp = i
+            break
+
+    count = -1
+    for j in range(temp, len(people)):
+        if people[j] <= limit:
+            if people[j] + people[count] <= limit:
+                people[count] = limit + 1
+                count -= 1
+                answer += 1
             else:
                 answer += 1
     return answer
